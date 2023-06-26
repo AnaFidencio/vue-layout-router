c76bd1ff5b44c9f6f5d47e601caaf3901c41a261
= <=>
2023-06-26 16:44:07 -0300

    aula

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,27 @@
 
 <template>
   <header>HEADER</header>
-  <aside>MENU</aside>
+  <aside>
+    <nav class="menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/contact">Contact</router-link>
+      <router-link to="/team">Team</router-link>
+      <router-link to="/services">Services</router-link>
+      <router-link to="/products">Products</router-link>
+    </nav>
+  </aside>
+  <main>
+    <router-view/>
+  </main>
   <main>MAIN</main>
   <footer>FOOTER</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+.menu{ 
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
+import TeamView from '../views/TeamView.vue'
+import ServicesView from '../views/ServicesView.vue'
+import ProductsView from '../views/ProductsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +18,30 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    }
+    ,
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    }
+    ,
+    {
+      path: '/team',
+      name: 'team',
+      component: TeamView
+    }
+    ,
+    {
+      path: '/services',
+      name: 'services',
+      component: ServicesView
+    }
+    ,
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductsView
     }
   ]
 })
